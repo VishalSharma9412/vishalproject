21017a551672a38496571e7a7c2e0d4cc3ae4996
VishalSharma9412 <83503016+VishalSharma9412@users.noreply.github.com>
2021-07-21 16:32:53 +0530

    Add files via upload

---
+++ src/Heading.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Heading = (props) => {
+   return <h3 > <h3/>
+}
---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function List(){
+    return ( <ol>
+    <li>Vishal</li>
+    <li>Sushma</li>
+    <li>Aditya</li>
+    <li>Ashok</li>
+</ol>  );
+}
+
+export default List;
